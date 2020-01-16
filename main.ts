08dc4db1e9dfd8c56adeea6140665dd5768acd83
Joseph Szymanski <59422896+JosephSzymanski@users.noreply.github.com>
2020-01-16 11:33:51 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+brick.showImage(images.expressionsBigSmile)
+forever(function () {
+    pause(100)
+    brick.showImage(images.expressionsMouth1open)
+    pause(100)
+    brick.showImage(images.expressionsHeartSmall)
+})
